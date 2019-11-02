fddc154e6dc72d6e47ff313f7be16566d605b303
Agustin Salinas <chaucharian@gmail.com>
2019-11-02 01:56:13 -0300

    real barber button look and feel done

--- src/components/realBarberButton.jsx
+++ src/components/realBarberButton.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { withStyles} from '@material-ui/styles';
+import { colorScheme } from '../styles/styles';
 
 const styles = {
     button: {
+        maxWidth: "400px",
         border: "2px solid #2e2e2e",
         cursor: "pointer",
         letterSpacing: "0.2125rem",
@@ -30,17 +32,50 @@ const styles = {
                     paddingLeft: "5px"
                 },
                 "& .mask1": {
-                    backgroundColor: "#d00909",
-                    transform: "translate3d(150%, -100px, 0) rotate3d(0, 0, 1, 90deg)"
+                    backgroundColor: "#FFF",
+                    transform: "translate3d(150%, -85px, 0) rotate3d(0, 0, 1, 110deg)",
+                    zIndex: 18
                 },
                 "& .mask2": {
-                    backgroundColor: "#FFF",
-                    transform: "translate3d(130%, -100px, 0) rotate3d(0, 0, 1, 90deg)"
+                    backgroundColor: colorScheme.blueBarberPost,
+                    transform: "translate3d(145%, -85px, 0) rotate3d(0, 0, 1, 110deg)",
+                    zIndex: 17
                 },
                 "& .mask3": {
-                    backgroundColor: "#012ea1",
-                    transform: "translate3d(115%, -100px, 0) rotate3d(0, 0, 1, 90deg)"
-                }
+                    backgroundColor: "#FFF",
+                    transform: "translate3d(140%, -85px, 0) rotate3d(0, 0, 1, 110deg)",
+                    zIndex: 16
+                },
+                "& .mask4": {
+                    backgroundColor: colorScheme.redBarberPost,
+                    transform: "translate3d(135%, -85px, 0) rotate3d(0, 0, 1, 110deg)",
+                    zIndex: 15
+                },
+                "& .mask5": {
+                    backgroundColor: "#FFF",
+                    transform: "translate3d(130%, -85px, 0) rotate3d(0, 0, 1, 110deg)",
+                    zIndex: 14
+                },
+                "& .mask6": {
+                    backgroundColor: colorScheme.blueBarberPost,
+                    transform: "translate3d(125%, -85px, 0) rotate3d(0, 0, 1, 110deg)",
+                    zIndex: 13
+                },
+                "& .mask7": {
+                    backgroundColor: "#FFF",
+                    transform: "translate3d(120%, -85px, 0) rotate3d(0, 0, 1, 110deg)",
+                    zIndex: 12
+                },
+                "& .mask8": {
+                    backgroundColor: colorScheme.redBarberPost,
+                    transform: "translate3d(115%, -85px, 0) rotate3d(0, 0, 1, 110deg)",
+                    zIndex: 11
+                },
+                "& .mask9": {
+                    backgroundColor: "#FFF",
+                    transform: "translate3d(110%, -85px, 0) rotate3d(0, 0, 1, 110deg)",
+                    zIndex: 10
+                },
             }
         },
          
@@ -82,6 +117,56 @@ const styles = {
                 transform: "translate3d(-120%, -50px, 0) rotate3d(0, 0, 1, 45deg)",
                 transition: "all 1.1s cubic-bezier(0.19, 1, 0.22, 1)",
                 width: "200px"
+            },
+            "& .mask4": {
+                backgroundColor: "#d00909",
+                backgroundColor: "rgba(255, 255, 255, 0.5)",
+                height: "100px",
+                position: "absolute",
+                transform: "translate3d(-120%, -50px, 0) rotate3d(0, 0, 1, 45deg)",
+                transition: "all 1.1s cubic-bezier(0.19, 1, 0.22, 1)",
+                width: "200px"
+            },
+            "& .mask5": {
+                backgroundColor: "#fff",
+                backgroundColor: "rgba(255, 255, 255, 0.5)",
+                height: "100px",
+                position: "absolute",
+                transform: "translate3d(-120%, -50px, 0) rotate3d(0, 0, 1, 45deg)",
+                transition: "all 1.1s cubic-bezier(0.19, 1, 0.22, 1)",
+                width: "200px"
+            },
+            "& .mask6": {
+                backgroundColor: "rgba(255, 255, 255, 0.5)",
+                height: "100px",
+                position: "absolute",
+                transform: "translate3d(-120%, -50px, 0) rotate3d(0, 0, 1, 45deg)",
+                transition: "all 1.1s cubic-bezier(0.19, 1, 0.22, 1)",
+                width: "200px"
+            },
+            "& .mask7": {
+                backgroundColor: "rgba(255, 255, 255, 0.5)",
+                height: "100px",
+                position: "absolute",
+                transform: "translate3d(-120%, -50px, 0) rotate3d(0, 0, 1, 45deg)",
+                transition: "all 1.1s cubic-bezier(0.19, 1, 0.22, 1)",
+                width: "200px"
+            },
+            "& .mask8": {
+                backgroundColor: "rgba(255, 255, 255, 0.5)",
+                height: "100px",
+                position: "absolute",
+                transform: "translate3d(-120%, -50px, 0) rotate3d(0, 0, 1, 45deg)",
+                transition: "all 1.1s cubic-bezier(0.19, 1, 0.22, 1)",
+                width: "200px"
+            },
+            "& .mask9": {
+                backgroundColor: "rgba(255, 255, 255, 0.5)",
+                height: "100px",
+                position: "absolute",
+                transform: "translate3d(-120%, -50px, 0) rotate3d(0, 0, 1, 45deg)",
+                transition: "all 1.1s cubic-bezier(0.19, 1, 0.22, 1)",
+                width: "200px"
             }
         }
     },
@@ -100,6 +185,12 @@ const RealBarberButton = (props) => {
             <div className="mask1"></div>
             <div className="mask2"></div>
             <div className="mask3"></div>
+            <div className="mask4"></div>
+            <div className="mask5"></div>
+            <div className="mask6"></div>
+            <div className="mask7"></div>
+            <div className="mask8"></div>
+            <div className="mask9"></div>
         </div>
     </button>);
 };
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -4,6 +4,8 @@ export const colorScheme = {
     buttonHover: "#641b22",
     buttonActive: "#000",
     grey: "#737373",
+    blueBarberPost: "#273cd2",
+    redBarberPost: "#e21111",
     disabledOptions: "#635e5e"
 };
 

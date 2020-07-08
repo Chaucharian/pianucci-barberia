4e2b5f337cd41fdb75045c43cea1863097f82a93
ASalinas <chaucharian@gmail.com>
2020-07-07 23:12:20 -0300

    hours improved

--- src/containers/scheduleHandler.jsx
+++ src/containers/scheduleHandler.jsx
@@ -95,8 +95,8 @@ const ScheduleHandler = props => {
     const { fetching, currentPage, user: { daysOff } } = state;
     const { from: morningFrom, to: morningTo } = morningSchedule;
     const { from: afternoonFrom, to: afternoonTo } = afternoonSchedule;
-    const defaultMorningTimeRange = ["6:00","7:00","8:00", "9:00", "10:00", "11:00", "12:00","13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "00:00"];
-    const defaultAfternoonTimeRange = ["6:00","7:00","8:00", "9:00", "10:00", "11:00", "12:00","13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "00:00"];
+    const defaultMorningTimeRange = ["7:00","8:00", "9:00", "10:00", "11:00", "12:00","13:00"];
+    const defaultAfternoonTimeRange = ["13:00", "14:00", "15:00", "16:00", "17:00", "18:00","19:00", "20:00", "21:00", "22:00", "23:00", "00:00"];
 
     const disableDates = date => date < new Date().getTime() || date > END_DATE || date < START_DATE || isDateDisabled(daysOff, date);
 
@@ -147,7 +147,7 @@ const ScheduleHandler = props => {
                             <Select label="Desde" selection={morningFrom} onChange={morningSelection}>
                                 {defaultMorningTimeRange}
                             </Select>
-                            <Select label="Hasta" selection={morningTo} onChange={morningSelection}>
+                            <Select label="Hasta" selection={afternoonTo} onChange={morningSelection}>
                                 {defaultMorningTimeRange}
                             </Select>
                         </div>
@@ -155,7 +155,7 @@ const ScheduleHandler = props => {
                     <div>
                         <h2>TARDE</h2>
                         <div className={classes.timeRange}>
-                            <Select label="Desde" selection={afternoonFrom} onChange={afternoonSelection}>
+                            <Select label="Desde" selection={morningFrom} onChange={afternoonSelection}>
                                 {defaultAfternoonTimeRange}
                             </Select>
                             <Select label="Hasta" selection={afternoonTo} onChange={afternoonSelection}>

2a6f90347782a87da56fd6a6306801724156b77a
ASalinas <chaucharian@gmail.com>
2020-07-09 14:59:49 -0300

    enable today selection

--- src/containers/scheduleHandler.jsx
+++ src/containers/scheduleHandler.jsx
@@ -98,7 +98,7 @@ const ScheduleHandler = props => {
     const defaultMorningTimeRange = ["","7:00","8:00", "9:00", "10:00", "11:00", "12:00","13:00"];
     const defaultAfternoonTimeRange = ["","13:00", "14:00", "15:00", "16:00", "17:00", "18:00","19:00", "20:00", "21:00", "22:00", "23:00", "00:00"];
 
-    const disableDates = date => date < new Date().getTime() || date > END_DATE || date < START_DATE;
+    const disableDates = date => date < new Date().getTime() || date > END_DATE;
 
     const changeTab = (event, nextTab) => {
         if(nextTab === 0) setCurrentDate(null); // set to initial state

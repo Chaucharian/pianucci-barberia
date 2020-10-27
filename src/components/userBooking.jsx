afb743ec968abf400f7fbae2b69ba1e0733baa3d
ASalinas <chaucharian@gmail.com>
2020-10-27 19:07:44 -0300

    other try

--- src/components/userBooking.jsx
+++ src/components/userBooking.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { withStyles } from '@material-ui/styles'
 import { getHours, isSameDay, format } from 'date-fns'
-import { ConfirmModal } from 'components/Modal/ConfirmModal'
+import { ConfirmModal } from 'components/Modal'
 
 const styles = {
     container: {
